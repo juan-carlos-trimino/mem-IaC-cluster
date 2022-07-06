3599f08e5e732ce7f7d0bf5f6286ed3fab8a086a
Juan Carlos Trimino <juancarlos@trimino.com>
2022-07-06 14:25:58 -0500

    Add variable for the limitations of the kubernetes_manifest resource.

--- tf/variables.tf
+++ tf/variables.tf
@@ -12,3 +12,21 @@ variable app_version {
   description = "The application version."
   default = "1.0.0"
 }
+
+# The limitations of the kubernetes_manifest resource
+# ---------------------------------------------------
+# If you want to create arbitrary Kubernetes resources in a cluster using Terraform, particularly
+# CRDs (Custom Resource Definitions), you can use the kubernetes_manifest resource from the
+# Kubernetes provider, but with these limitations:
+# (1) This resource requires API access during the planning time. This means the cluster has to be
+#     accessible at plan time and thus cannot be created in the same apply operation. That is, it
+#     is required to use two (2) separate Terraform apply steps: (1) Provision the cluster;
+#     (2) Create the resource.
+# (2) Any CRD (Custom Resource Definition) must already exist in the cluster during the planning
+#     phase. That is, it is required to use two (2) separate Terraform apply steps: (1) Install the
+#     CRDs; (2) Install the resources that are using the CRDs.
+# This are Terraform limitations, not specific to Kubernetes.
+variable k8s_manifest_crd {
+  type = bool
+  default = "true"
+}

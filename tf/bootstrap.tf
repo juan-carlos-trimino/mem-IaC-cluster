d56054261a1b5e2d56ecea25520b20f444ae853c
Juan Carlos Trimino <juancarlos@trimino.com>
2023-04-25 11:04:44 -0500

    Change RabbitMQ to use more than one replica.

--- tf/bootstrap.tf
+++ tf/bootstrap.tf
@@ -599,76 +599,35 @@ module "mem-mongodb" {
 ###################################################################################################
 # rabbitmq                                                                                        #
 ###################################################################################################
-/*** rabbitmq - deployment
-# Deployment.
-module "mem-rabbitmq" {
-  # Specify the location of the module, which contains the file main.tf.
-  source = "./modules/rabbitmq-deploy"
-  # dir_name = "../../${local.svc_rabbitmq}/rabbitmq"
-  app_name = var.app_name
-  app_version = var.app_version
-  # This image has the RabbitMQ dashboard.
-  image_tag = "rabbitmq:3.9.15-management-alpine"
-  image_pull_policy = "IfNotPresent"
-  # image_tag = "rabbitmq:3.9.7-alpine"
-  # path_rabbitmq_files = "./utility-files/rabbitmq"
-  path_rabbitmq_files = "./modules/rabbitmq-statefulset/util"
-  namespace = local.namespace
-  qos_limits_cpu = "400m"
-  qos_limits_memory = "300Mi"
-  rabbitmq_erlang_cookie = var.rabbitmq_erlang_cookie
-  rabbitmq_default_pass = var.rabbitmq_default_pass
-  rabbitmq_default_user = var.rabbitmq_default_user
-  # cr_login_server = local.cr_login_server
-  # cr_username = var.cr_username
-  # cr_password = var.cr_password
-  amqp_service_port = 5672
-  amqp_service_target_port = 5672
-  # HTTP API clients, management UI, and rabbitmqadmin, without and with TLS (only if the
-  # management plugin is enabled).
-  mgmt_service_port = 15672
-  mgmt_service_target_port = 15672
-  service_name = local.svc_rabbitmq
-}
-***/  # rabbitmq - deployment
-
-# /*** rabbitmq - statefulset goodxxcxx
-# StatefulSet.
+# /*** rabbitmq
 module "mem-rabbitmq" {
   count = var.k8s_manifest_crd ? 0 : 1
   source = "./modules/rabbitmq-statefulset"
   app_name = var.app_name
   app_version = var.app_version
   # This image has the RabbitMQ dashboard.
-  image_tag = "rabbitmq:3.9.7-management-alpine"
+  image_tag = "rabbitmq:3.11.13-management-alpine"
+  # This image does not have the RabbitMQ dashboard.
+  # image_tag = "rabbitmq:3.11.13-alpine"
   image_pull_policy = "IfNotPresent"
+  namespace = local.namespace
   path_rabbitmq_files = "./modules/rabbitmq-statefulset/util"
-  #
   rabbitmq_erlang_cookie = var.rabbitmq_erlang_cookie
   rabbitmq_default_pass = var.rabbitmq_default_pass
   rabbitmq_default_user = var.rabbitmq_default_user
-  #
   publish_not_ready_addresses = true
-  namespace = local.namespace
   # Because several features (e.g. quorum queues, client tracking in MQTT) require a consensus
   # between cluster members, odd numbers of cluster nodes are highly recommended: 1, 3, 5, 7
   # and so on.
-  replicas = 1
+  replicas = 3
   # Limits and requests for CPU resources are measured in millicores. If the container needs one
   # full core to run, use the value '1000m.' If the container only needs 1/4 of a core, use the
   # value of '250m.'
-  qos_limits_cpu = "400m"
-  qos_limits_memory = "300Mi"
+  qos_limits_cpu = "500m"
+  qos_limits_memory = "400Mi"
   pvc_access_modes = ["ReadWriteOnce"]
-  pvc_storage_size = "5Gi"
+  pvc_storage_size = "10Gi"
   pvc_storage_class_name = "ibmc-block-silver"
-  # Used by AMQP 0-9-1 and AMQP 1.0 clients without and with TLS.
-  amqp_service_port = 5672
-  amqp_service_target_port = 5672
-  # HTTP API clients, management UI, and rabbitmqadmin, without and with TLS (only if the
-  # management plugin is enabled).
-  mgmt_service_port = 15672
-  mgmt_service_target_port = 15672
   env = {
     # If a system uses fully qualified domain names (FQDNs) for hostnames, RabbitMQ nodes and CLI
     # tools must be configured to use so called long node names.
@@ -676,6 +635,10 @@ module "mem-rabbitmq" {
     # Override the main RabbitMQ config file location.
     RABBITMQ_CONFIG_FILE = "/config/rabbitmq"
   }
+  amqp_service_port = 5672
+  amqp_service_target_port = 5672
+  mgmt_service_port = 15672
+  mgmt_service_target_port = 15672
   service_name = local.svc_rabbitmq
 }
 # ***/  # rabbitmq - statefulset
@@ -722,7 +685,7 @@ module "mem-gateway" {
     success_threshold = 1
   }]
   service_name = local.svc_gateway
-  # service_type = "LoadBalancer"
+  service_type = "LoadBalancer"
 }
 
 module "mem-history" {
